0942fba6c29d28768e8d7104183ff16292bcd892
ogios <2134692955@qq.com>
2024-07-27 17:07:19 +0800

    remove eww

--- eww/scripts/get_kb_layout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
-  stdbuf -o0 awk -F '>>|,' -e '/^activelayout>>/ {print tolower(substr($3, 1, 2))}'
