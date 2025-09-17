fd1d46e3e21a093e4d72f790e9566c54b95f58b7
ogios <ogios@foxmail.com>
2025-09-17 16:30:27 +0800

    new workflow

--- ags/app.ts
+++ ags/app.ts
@@ -9,18 +9,18 @@ App.start({
   css: style,
   main() {
     App.get_monitors().map((m) => SystemMonitor(m, "ags-backdrop"));
-    App.get_monitors().map((m) => SystemMonitor(m, "ags-front"));
+    // App.get_monitors().map((m) => SystemMonitor(m, "ags-front"));
 
     App.get_monitors().map((m) => Time(m, "ags-backdrop"));
-    App.get_monitors().map((m) => Time(m, "ags-front"));
+    // App.get_monitors().map((m) => Time(m, "ags-front"));
 
-    App.get_monitors().map(PoweMenu.window);
-    App.get_monitors().map(TogglePowerMenu.TogglePowerMenu);
+    // App.get_monitors().map(PoweMenu.window);
+    // App.get_monitors().map(TogglePowerMenu.TogglePowerMenu);
   },
-  requestHandler(request: string, res) {
-    if (request === "power-menu") {
-      PoweMenu.toggle();
-    }
-    res("");
-  },
+  // requestHandler(request: string, res) {
+  //   if (request === "power-menu") {
+  //     PoweMenu.toggle();
+  //   }
+  //   res("");
+  // },
 });
